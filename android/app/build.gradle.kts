f060a733dc5338e05c9c50800276a196b50912b7
MOONFISH <xiang130911@163.com>
2026-05-20 08:44:00 +0800

    Fix Gradle build errors - remove problematic configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,10 +15,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.harbeat.harbeat_app"
@@ -26,8 +22,6 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
     }
 
     buildTypes {
